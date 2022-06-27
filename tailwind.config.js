df82d7fc3d8324437f359e614a16a306b907f7c8
Ashish rana <ashishrana288@gmail.com>
2022-06-27 22:10:59 +0530

    Update tailwind.config.js
    tailwind css changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false,
-  purge: [
-    './playground/index.html',
-    './playground/src/**/*.{vue,js,ts,jsx,tsx}'
-  ],
+  content: [],
   theme: {
     extend: {
-      zIndex: {
-        '-1': '-1'
-      },
-      container: {
-        center: true,
-        padding: '2rem'
+      screens: {
+        "3xl": "2000px",
       }
-    }
+    },
   },
-  variants: {
-    extend: {}
-  },
-  plugins: [require('./src/index')]
-}
+  plugins: [require('tailwind-scrollbar-hide')],
+};
